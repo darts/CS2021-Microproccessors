2c592a5fa85d67f5b0e03b39e67642fce6cbf2dc
Efeosa Eguavoen <33104551+foesa@users.noreply.github.com>
2019-03-08 18:07:24 +0000

    Added files with code for doing calculations

--- Assignment-2/App.s
+++ Assignment-2/App.s
@@ -1,6 +1,9 @@
-	AREA	buttonUp, CODE, READONLY
+	AREA	AsmTemplate, CODE, READONLY
 	IMPORT	main
-		
+
+; sample program makes the 4 LEDs P1.16, P1.17, P1.18, P1.19 go on and off in sequence
+; (c) Mike Brady, 2011 -- 2019.
+
 	EXPORT	start
 start
 
@@ -8,69 +11,24 @@ IO1DIR	EQU	0xE0028018
 IO1SET	EQU	0xE0028014
 IO1CLR	EQU	0xE002801C
 IO1PIN	EQU	0xE0028010
-	
-	LDR R1, =IO1DIR
-	LDR R2, =0x000F0000		; select P1.19 through P1.16
-	STR R2, [R1]			; set as outputs
-	LDR R1, =IO1SET			; 
-	STR R2, [R1]			; set the bits to turn off the LEDs
-	ldr r2, =IO1CLR
-	ldr r3, =IO1PIN
-	
-foreverLoop
-	BL getPress
-	
-	;ldr r4, [r3]
-	;ldr r6, = 0x00F00000
-	;and r4, r4, r6
-	;eor r4, r4, r6
-	;LDR R0, =IO1PIN		; load button addr
-	;LDR R0, [R0]		; load originalValue
-	;LDR R1, =0x00F00000	; load mask
-	;AND R0, R1, R0		; originalValue &= mask
-	;EOR R0, R0, R1
-	
-	
-	CMP R0, #-20
-	BNE foreverLoop1
-	LDR R0, =-8
-	BL dispNum
-	B foreverLoop
-foreverLoop1
-	CMP R0, #-21
-	BNE foreverLoop2
-	LDR R0, =4
-	BL dispNum
-	B foreverLoop
-foreverLoop2
-	CMP R0, #-22
-	BNE foreverLoop3
-	LDR R0, =2
-	BL dispNum
-	B foreverLoop
-foreverLoop3	
-	CMP R0, #-23
-	BNE foreverLoop
-	LDR R0, =1
-	BL dispNum
-	B foreverLoop
 
 
-stop B stop
 
-;Returns pressed key in R0
 getPress
-	STMFD sp!, {R1-R5, LR}
+	STMFD sp, {R1-R5, LR}
 	; R0 = original values
 	; R1 = mask
 	; R2 = isLongPress
 	; R3 = timerDelay
 	; R4 = button addr
 	; R5 = button values
-	
+	; R6 = num1
+	; R7 = num2
+	; R8 = first boolean
+	; R9 = operand
 	LDR R0, =IO1PIN		; load button addr
 	LDR R0, [R0]		; load originalValue
-	LDR R1, =0x00F00000	; load mask
+	LDR R1, 0xF00000	; load mask
 	AND R0, R1, R0		; originalValue &= mask
 	
 	CMP R1, R0			; if(button values == mask)
@@ -79,7 +37,7 @@ getPress
 	B notNegGetPress	; }
 pressedGetPress			; else{
 	LDR R2, =1			; 		isLongPress = 1
-	LDR R3, =1100000	; 		timerDelay
+	LDR R3, =4000000	; 		timerDelay
 	LDR R4, =IO1PIN		;		load button addr
 
 waitTimeGetPress
@@ -90,16 +48,10 @@ waitTimeGetPress
 	CMP R5, R0			;			if (original values != button values){
 	BEQ waitTimeGetPress;				isLongPress = false
 	LDR R2, =0			;				break
-	B shortPressGetPress;
 doneWaitGetPress		;			}
-	LDR R5, [R4]		; 			load button values()
-	AND R5, R5, R1		;			button values &= mask
-	CMP R5, R0			;			while(button values == originalValues)
-	BEQ doneWaitGetPress;			{wait()}
-shortPressGetPress
-	EOR R0, R0, R1		; 		originalValue XOR mask
+						;		}
 	LSR R0, #20			; 		originalValue >> 20
-	
+	AND R0, R0, R1		; 		originalValue &= mask
 	CMP R0, #1			; 		if(originalValue == 1)
 	BNE chkTwoGetPress	; 		{
 	LDR R0, =20			; 			originalValue = 20
@@ -122,42 +74,91 @@ mayNegGetPress			; 		}
 	NEG R0, R0			; 			originalValue = 2'sComplementOf(originalValue)
 notNegGetPress			; 		}
 
+increment               ;     
+	CMP R0, #20         ; if(button == increase)
+	BNE decrement       ; {
+	CMP R6,#7			;	if(num1>= 7){
+	BGE	over 			;		B to over}
+	ADD R6,R6,#1        ; 		num1++
+	B finish            ; }
+decrement               ;
+	CMP R0,#21          ; elif(button == decrease)
+	BNE adder           ;{
+	CMP R6,#-8			; 	if(num1<= -8){
+	BLE over			;		B to over} 
+	SUB R6,R6,#1        ;		num--
+	B finish            ;}
+adder                   ;
+	CMP R0,#22          ;elif(button == add)
+	BNE subtract        ;{
+	CMP R8, #0 			;	if(first == true)
+	BNE adder2			;		{
+	MOV R7,R6			;			num2 = num1
+	MOV R6,#0			;			num1 =0
+	//clear display code goes here ; display
+	MOV R8, #1			;			first = false;	
+	B endAdd			;	}
+adder2					;	else {
+	CMP R9,#0			;		if(operand == 0)
+	BNE insub			;		{
+	ADD R7,R6,R7		;		num2 = num1 + num2	
+	MOV R6,#0			; 		num1 = 0
+	//displaythis		;   	display result
+	B endAdd			;		}
+insub
+	CMP R9,#1			;		elif(operand == 1)
+	BNE subber			;		{
+	SUB R7,R7,R6		;			num2 = num2-num1
+	MOV R6,#0			;			num1 = 0;
+	//display			;			displayResult;
+	B endAdd			;		}
+endAdd
+	MOV R9,#0    		;		operand = 0;
+	B finish			;}
+subber
+	CMP R0,#23          ;elif(button == sub)
+	BNE longPressAdd        ;{
+	CMP R8, #0 			;	if(first == true)
+	BNE subber2			;		{
+	MOV R7,R6			;			num2 = num1
+	MOV R6,#0			;			num1 =0
+	//clear display code goes here ; display
+	MOV R8, #1			;			first = false;	
+	B endsub			;	}
+subber2					;	else {
+	CMP R9,#0			;		if(operand == 0)
+	BNE insub1			;		{
+	ADD R7,R6,R7		;		num2 = num1 + num2	
+	MOV R6,#0			; 		num1 = 0
+	//displaythis		;   	display result
+	B endsub			;		}
+insub1
+	CMP R9,#1			;		elif(operand == 1)
+	BNE subber			;		{
+	SUB R7,R7,R6		;			num2 = num2-num1
+	MOV R6,#0			;			num1 = 0;
+	//display			;			displayResult;
+	B endsub			;		}
+endAdd
+	MOV R9,#1    		;		operand = 0;
+	B finish			;}
+longPressAdd
+	CMP R0,#-22			
+	BNE longPressSub
+	MOV R9,#3
+	MOV R6,#0
+	B finish
+longPressSub
+	CMP R0,#-23
+	BNE finish
+	MOV R6,#0
+	MOV R7,#0
+	MOV R8,#0
+	MOV R9,#0
+	B finish
+over
+	BL flashNum
+finish
 	LDMFD sp!, {R1-R5, PC}	
-	
-;Takes number in R0
-;Num in range 
-dispNum
-	STMFD sp!, {R1-R2, LR}
-	LDR R1, =IO1SET
-	LDR R2, =0x000F0000
-	STR R2, [R1]
-	
-	LDR R1, =IO1CLR
-	ADD r0, r0, #8
-	ldr r2, =dispArray
-	ldr r2, [r2, r0]
-	LSL R2, #16
-	STR R2, [R1]
-	LDMFD sp!, {R1-R2, PC}
-	
-	AREA TestData, DATA, READWRITE
 
-dispArray
-	DCB 2_0001
-	DCB 2_1001
-	DCB 2_0101
-	DCB 2_1101
-	DCB 2_0011
-	DCB 2_1011
-	DCB 2_0111
-	DCB 2_1111
-	DCB 2_0000
-	DCB 2_1000
-	DCB 2_0100
-	DCB 2_1100
-	DCB 2_0010
-	DCB 2_1010
-	DCB 2_0110
-	DCB 2_1110
-		
 	END
